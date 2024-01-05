262c327e0166e1a467c7c7a102d48fd36d5e3449
Akshay Jain <akshay22.11.1995@gmail.com>
2024-01-05 13:41:33 +0530

    main function

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
 import pandas as pd
 from result_collector import final_results
 from give_me_q import q_generator
+import time
+import sys
 
-try:
-    print('----------------Excecution Begins----------------')
-    name_list=q_generator()
-except Exception as e:
-    print(f'Error in reading list: {e}')
-else:
-    results = final_results(name_list)
-    print('---------------Excecution Complete---------------')
+def main(sport):
+    try:
+        print('----------------Excecution Begins----------------')
+        name_list=q_generator(sport)
+    except Exception as e:
+        print(f'Error in reading list: {e}')
+    else:
+        results = final_results(name_list,sport)
+        print('---------------Excecution Complete---------------')
+
+if __name__ == "__main__":
+  sport = sys.argv[1]
+  main(sport)
